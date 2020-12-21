5d7f2b629999f00dec54fbf6e8e3de6d73705f19
Michael Abah <donsmartez@gmail.com>
2020-12-21 20:47:41 +0100

    added different colors to text input

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,18 @@
 import  React, {Component} from "react";
 
 class Card extends Component {
+
     render(){
+      const random1 = Math.random() *255;
+      const random2 = Math.random() *255;
+      const random3 = Math.random() *255;
       
       const profile = this.props;
       return (
         <div className="github-profile">
           <img alt={`${profile.name}'s profile`} src={profile.avatar_url}/>
           <div className="info">
-            <div className="name" style={{color: Math.random() < 0.5 ? "green": "blue"}}> {profile.name}</div>
+            <div className="name" style={{color: `rgb(${random1}, ${random2},${random3})`}}> {profile.name}</div>
             <div className="company"> {profile.company}</div>
           </div>
         </div>
